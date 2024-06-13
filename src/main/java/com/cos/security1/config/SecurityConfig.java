97905f8195d63d7bda7c5bbc262aba693ad63625
noahmik <s46422574@gmail.com>
2024-06-13 12:20:06 +0900

    FIX :: SecurityConfig 수정

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -4,12 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity //스프링 필터체인 등록
 public class SecurityConfig {
+    @Bean //해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -17,11 +22,19 @@ public class SecurityConfig {
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/user/**").authenticated() // /user/** 패턴은 인증 필요
-                                .requestMatchers("/manager/**").hasRole("ADMIN") // /manager/** 패턴은 ADMIN 권한 필요
+                                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // /manager/** 패턴은 ADMIN 또는 MANAGER 권한 필요
                                 .requestMatchers("/admin/**").hasRole("ADMIN") // /admin/** 패턴은 ADMIN 권한 필요
                                 .anyRequest().permitAll() // 그 외의 요청은 모두 허용
                 )
-                .formLogin(withDefaults());
+                .formLogin(form ->
+                        form
+                                .loginPage("/loginForm")
+                                .permitAll()
+                )
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .accessDeniedPage("/loginForm") // 접근 거부 시 /login으로 리다이렉션
+                );
 
         return http.build();
     }
---
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package com.cos.security1.repository;public class UserRepository {
+}
